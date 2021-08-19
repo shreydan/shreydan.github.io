9c9e340ae9ae5b12e237339c3a86b17c336325a5
Shreyas Daniel <iamshreydan@gmail.com>
2021-08-19 18:15:38 +0530

    update mobile

--- src/Components/Mobile.jsx
+++ src/Components/Mobile.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import styled from 'styled-components'
+import Card from './Card'
+import Header from './Header'
 
-function Mobile() {
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    margin: 5% 0;
+`
+const Content = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 90%;
+`
+
+const Cards = styled.div`
+    margin: 3% 0;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    padding-bottom: 3%;
+    border-bottom: 3px solid #757575;
+`
+
+
+function Mobile({links}) {
     return (
-        <div></div>
+        <Container>
+            <Content>
+                <Header />
+                <Cards>
+                    {
+                        links.map((link, key) => (
+                            <Card link={link} key={key} />
+                        ))
+                    }
+                </Cards>
+            </Content>
+        </Container>
     )
 }
 
