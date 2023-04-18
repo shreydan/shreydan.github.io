ab581a8288ab3a342bf3ff98a6a9f6d77f236c12
shreydan <iamshreydan@gmail.com>
2023-04-19 00:54:53 +0530

    basic header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<style>
+* {
+  box-sizing: border-box;
+}
+
+:global(body),
+:global(html) {
+  width: 100%;
+  margin: 0;
+  font-family: monospace;
+}
+
+.title {
+  margin-left: 5%;
+  margin-top: 5%;
+}
+
+.title span {
+  color: #969696;
+}
+
+.title .tagline {
+  margin-top: -1%;
+}
+</style>
+
+<div class="title">
+  <h1>Shreyas Daniel <span>Gaddam</span></h1>
+  <div class="tagline">Machine Learning Engineer and Researcher</div>
+</div>
