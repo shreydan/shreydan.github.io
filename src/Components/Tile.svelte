6eb80d7e2686373b64bfb253abc714ad422d96e3
shreydan <iamshreydan@gmail.com>
2023-04-25 21:52:37 +0530

    update tile style

--- src/Components/Tile.svelte
+++ src/Components/Tile.svelte
@@ -1,20 +1,32 @@
 <style>
 .tile {
   min-height: 10rem;
-  width: 10rem;
+  min-width: 10rem;
   background-color: var(--bg-color);
-  border-radius: calc(0.2237 * 10rem);
-  display: grid;
-  place-items: center;
-  box-shadow: inset 0px 0px 0px 3px rgba(0, 0, 0, 0.1);
+  border-radius: calc(0.2237 * 12rem);
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+  box-shadow: inset 0px 0px 0px 2px rgba(0, 0, 0, 0.1),
+    0px 7px 7px -7px rgba(60, 60, 60, 0.1);
+
   text-decoration: none;
   color: black;
+  padding: 2rem;
+  transition-duration: 0.5s;
+  cursor: pointer;
 }
+
+.tile:hover {
+  filter: saturate(1.5);
+  transition-duration: 0.2s;
+}
+
 .tile_content {
   display: flex;
   flex-direction: column;
-  width: 70%;
-  height: 70%;
+  align-items: flex-start;
+  justify-content: flex-start;
 }
 .tile_content * {
   margin: 0;
@@ -22,12 +34,12 @@
 </style>
 
 <script>
-export let bg = "#e2e2e2";
+export let bg = "#f2fafc";
 export let title = "Blank Tile.";
 </script>
 
-<a class="tile" style="--bg-color: {bg};" href="https://google.com">
+<div class="tile" style="--bg-color: {bg};">
   <div class="tile_content">
     <h4>{title}</h4>
   </div>
-</a>
+</div>
