e3ccea07b7a73ffa9f9522a0acda7cff4e65f832
shreydan <iamshreydan@gmail.com>
2023-04-25 21:52:43 +0530

    better grid layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,8 @@
   width: 100%;
   margin: 0;
   font-family: monospace;
+  display: flex;
+  flex-direction: column;
 }
 
 .title {
@@ -22,9 +24,37 @@
 .title .tagline {
   margin-top: -1%;
 }
+
+.content {
+  align-self: center;
+  margin: 0;
+  width: 60vw;
+  margin-top: 5%;
+
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 14rem);
+  justify-content: center;
+  gap: 2rem;
+
+  margin-bottom: 10rem;
+}
 </style>
+
+<script>
+import Tile from "../Components/Tile.svelte";
+</script>
 
 <div class="title">
   <h1>Shreyas Daniel <span>Gaddam</span></h1>
   <div class="tagline">Machine Learning Engineer and Researcher</div>
+</div>
+
+<div class="content">
+  <Tile />
+  <Tile />
+  <Tile />
+  <Tile />
+  <Tile />
+  <Tile />
+  <Tile />
 </div>
